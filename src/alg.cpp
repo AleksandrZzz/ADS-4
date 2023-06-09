2f582b5451d9417a0ac40aab628e8def4e6a14db
AleksandrZzz <124875504+AleksandrZzz@users.noreply.github.com>
2023-06-09 17:55:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,3 @@
-// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int obj = 0;
   for (int i = 0; i < len; i++) {
@@ -30,37 +29,43 @@ int countPairs3(int *arr, int len, int value) {
     while (start < stop - 1) {
       int mid = (start + stop) / 2;
       if (i != mid && (arr[i] + arr[mid] == value)) {
-    int q = i, z = len;
-    while (1 < z - q) {
-      int qaz = (q + z) / 2;
-      if (arr[i] + arr[qaz] == value) {
-        obj++;
-        for (int j = mid + 1; (j < stop)
-             && (arr[i] + arr[j] == value)
-             && (mid != i); j++) {
-        int k = qaz + 1;
-        while (arr[i] + arr[k] == value && k < z) {
-          obj++;
-          k++;
-        }
-        for (int j = mid - 1; (j > start)
-             && (arr[i] + arr[j] == value)
-             && (mid != i); j--) {
-        k = qaz - 1;
-        while (arr[i] + arr[k] == value && k > q) {
-          obj++;
-          k--;
-        }
-        break;
-      }
-      if (arr[i] + arr[mid] > value) stop = mid;
-      else
-        start = mid;
-      if (arr[i] + arr[qaz] > value) {
-        z = qaz;
-      } else {
-        q = qaz;
-      }
-    }
+		int q = i, z = len;
+		while (1 < z - q) {
+		  int qaz = (q + z) / 2;
+		  if (arr[i] + arr[qaz] == value) {
+			obj++;
+			for (int j = mid + 1; (j < stop)
+				 && (arr[i] + arr[j] == value)
+				 && (mid != i); j++) {
+				int k = qaz + 1;
+				while (arr[i] + arr[k] == value && k < z) {
+				  obj++;
+				  k++;
+				}
+				for (int j = mid - 1; (j > start)
+					 && (arr[i] + arr[j] == value)
+					 && (mid != i); j--) {
+					k = qaz - 1;
+					while (arr[i] + arr[k] == value && k > q) {
+					  obj++;
+					  k--;
+					}
+					break;
+				  }
+				  
+				  if (arr[i] + arr[mid] > value) stop = mid;
+				  else
+					start = mid;
+				  if (arr[i] + arr[qaz] > value) {
+					z = qaz;
+				  } else {
+					q = qaz;
+				  }
+				}
+			  }
+		}
+	  }
+	}
   }
-  return obj;
+			return obj;
+}
