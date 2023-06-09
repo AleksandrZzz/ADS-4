149d5af40fd427cbdcf4466320ba96b17b7bf59b
AleksandrZzz <124875504+AleksandrZzz@users.noreply.github.com>
2023-06-09 18:01:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,54 +1,55 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   return 0;
-  int obj = 0;
-  for (int i = 0; i < len; i++) {
+  int Obj = 0;
+  for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
-        obj++;
+        Obj++;
       }
     }
   }
-  return obj;
+  return Obj;
 }
-
 int countPairs2(int *arr, int len, int value) {
   return 0;
-  int obj = 0;
+  int Obj = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = len - 1; j > i; j--) {
-      if (arr[i] + arr[j] == value)
-        obj++;
+      if (arr[i] + arr[j] == value) {
+        Obj++;
+      }
     }
   }
-  return obj;
+  return Obj;
 }
-
 int countPairs3(int *arr, int len, int value) {
   return 0;
-  int obj = 0;
+  int Obj = 0;
   for (int i = 0; i < len - 1; i++) {
-    int start = i, stop = len;
-    while (start < stop - 1) {
-      int mid = (start + stop) / 2;
-      if (i != mid && (arr[i] + arr[mid] == value)) {
-        obj++;
-        for (int j = mid + 1; (j < stop)
-             && (arr[i] + arr[j] == value)
-             && (mid != i); j++) {
-          obj++;
+    int left = i;
+    int right = len;
+    while (right > left + 1) {
+      int newmid = (right + left) / 2;
+      if (value < arr[i] + arr[newmid]) {
+        right = newmid;
+      } else if (value > arr[i] + arr[newmid]) {
+        left = newmid;
+      } else {
+        Obj++;
+        int count = newmid + 1;
+        while (count < right && arr[i] + arr[count] == value) {
+          Obj++;
+          count++;
         }
-        for (int j = mid - 1; (j > start)
-             && (arr[i] + arr[j] == value)
-             && (mid != i); j--) {
-          obj++;
+        count = newmid - 1;
+        while (count > left && arr[i] + arr[count] == value) {
+          Obj++;
+          count--;
         }
         break;
       }
-      if (arr[i] + arr[mid] > value) stop = mid;
-      else
-        start = mid;
     }
   }
-  return obj;
+  return Obj;
 }
